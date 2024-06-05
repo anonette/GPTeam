849fd7c0fb9daf71ce23a7c6037004a34f202962
dk <denisa.kera@gmail.com>
2024-06-05 18:12:34 +0300

    revert to poerty

--- src/utils/model_name.py
+++ src/utils/model_name.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 class ChatModelName(Enum):
     TURBO = "gpt-3.5-turbo"
-    GPT4 = "gpt-4"
-    CLAUDE = "claude-v1"
+    GPT4 = "gpt-4o"
+    CLAUDE = "claude-3-haiku-20240307"
     CLAUDE_INSTANT = "claude-instant-v1"
     WINDOW = "window"
